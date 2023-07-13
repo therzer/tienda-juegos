8300881090d5bf3b238c5cba308f86f10400fdba
Ezequiel Villalba <60276551+therzer@users.noreply.github.com>
2023-07-13 16:48:57 -0300

    img

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,6 +1,16 @@
+import Logo from "../../assets/img/logo.png";
+
 function Menu() {
     return (
-        <div>Menu</div>
+        <div className="menu-container">
+            <div className="menu-left">
+                <img src={Logo} alt="Logo" className="logo" />
+                <ul className="items-menu">
+                    <li className="item-menu" >Inicio</li>
+                    <li className="item-menu" >Categoria</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
---
+++ src/data/featuredGameData.js
@@ -0,0 +1,42 @@
+import img_1 from "../assets/img/featuredGame/Elden_Ring.jpg";
+import img_2 from "../assets/img/featuredGame/Red-Dead-2.jpg";
+import img_3 from "../assets/img/featuredGame/S04_MWII_Meta.jpg";
+
+export const featuredGameData = [
+    {
+        title:"Call of Duty®: Modern Warfare® II",
+        detail:"En Call of Duty®: Modern Warfare® II, los jugadores se verán inmersos en un conflicto a escala global sin precedentes que incluye el regreso de Operadores icónicos de la fuerza operativa 141.",
+        img: img_3,
+        price: "ARS$ 6.990,00",
+        tag:{
+            FPS:"FPS",
+            multiPlayer: "Multijugador",
+            action: "Acción"
+        }
+    },
+
+    {
+        title:"Red Dead Redemption 2",
+        detail:"Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es la épica historia de Arthur Morgan y la banda de Van der Linde, que huyen por toda América en el albor de una nueva era. También incluye acceso al mundo multijugador compartido de Red Dead Online",
+        img: img_2,
+        price: "ARS$ 11.299,00",
+        tag:{
+            FPS:"FPS",
+            multiPlayer: "Multijugador",
+            action: "Acción",
+            adventure: "Aventura",
+        }
+    },
+
+    {
+        title:"ELDEN RING",
+        detail:"EL NUEVO JUEGO DE ROL Y ACCIÓN DE AMBIENTACIÓN FANTÁSTICA. Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden y encumbrarte como señor del Círculo en las Tierras Intermedias.",
+        img: img_1,
+        price: "ARS$ 8.599,00",
+        tag:{
+            darkfantasy:"Fantasia oscura",
+            rol: "Rol",
+            action: "Acción"
+        }
+    },
+]
